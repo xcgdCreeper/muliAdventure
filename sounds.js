e38b5da5b33615e6ffcdadfa1d5dfadf7fc26987
xcgdCreeper <57204065+xcgdCreeper@users.noreply.github.com>
2022-04-22 10:52:53 +0800

    小游戏

---
+++ sounds.js
@@ -0,0 +1,14 @@
+//游戏结束音效
+var soundList=['./sounds/cry.mp3','./sounds/donkey.mp3','./sounds/yada.mp3','./sounds/po.mp3','./sounds/muli.mp3'];
+//吃到食物音效
+var eatList=['./sounds/en.mp3','./sounds/ma.mp3','./sounds/ah.mp3','./sounds/ba.mp3','./sounds/yue.mp3'];
+//游戏背景音乐
+var music='./sounds/Merry Christmas Mr. Lawrence.mp3';
+//战败提示
+var loseText="你输了，德川试图逃跑";
+//战败图片
+var losePic="./lose.png";
+//地图大小
+var rowCount = 10, colCount = 10;
+//游戏速度 ms/帧
+var speed = 250;
